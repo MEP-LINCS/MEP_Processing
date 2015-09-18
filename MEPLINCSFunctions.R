7d29a0d718af1902a17c86cde4378ab6bfe8ca0c
Mark Dane <dane@ohsu.edu>
2015-09-18 08:37:53 -0700

    Integrated analysis first pass

--- MEPLINCSFunctions.R
+++ MEPLINCSFunctions.R
@@ -1,6 +1,48 @@
 #MEP-LINCS Analysis functions
 #Mark Dane 9/2015
 
+#Preprocessing Functions
+
+#Functions to create or expose in MEMA
+calcTheta <- function(x,y) {
+  z <- x + 1i * y
+  res <- 90 - Arg(z) / pi * 180
+  res %% 360
+}
+
+spotCellDensities<- function (spot, radius = (max(spot$Cells_Location_Center_X) - min(spot$Cells_Location_Center_X))/5) 
+{
+  distMatrix <- as.matrix(dist(spot[, list(Cells_Location_Center_X, Cells_Location_Center_Y)]))
+  count <- apply(distMatrix, 2, function(x) {
+    sum(x <= radius) - 1
+  })
+  cellDensity <- count/(pi * radius^2)
+  return(cellDensity)
+}
+
+cellNeighbors<- function (spot, radius = (max(spot$Nuclei_CP_AreaShape_Center_X) - min(spot$Nuclei_CP_AreaShape_Center_X))/5) 
+{
+  distMatrix <- as.matrix(dist(spot[, list(Nuclei_CP_AreaShape_Center_X, Nuclei_CP_AreaShape_Center_Y)]))
+  count <- apply(distMatrix, 2, function(x) {
+    sum(x <= radius) - 1
+  })
+  return(count)
+}
+
+medianNorm <- function(DT, value){
+  normedValues <- DT[, value, with = FALSE]/median(unlist(DT[, value, with = FALSE]), na.rm=TRUE)
+}
+
+normDataset <- function(dt){
+  parmNormedList <- lapply(grep("_CP_|_QI_|_PA_|SpotCellCount|Lineage",colnames(dt),value = TRUE), function(parm){
+    dt <- dt[,paste0(parm,"_MedNorm") := normWellsWithinPlate(.SD, value=parm, baseECM = ".*",baseGF = "FBS"), by="Barcode"]
+    #     parmNormed <- pcDT[,normWellsWithinPlate(.SD, value=parm, baseECM = ".*",baseGF = "HighSerum"), by="Barcode"]
+    #     parmNormed <- parmNormed[,Barcode := NULL]
+    return(dt)
+  })
+  return(parmNormedList[[length(parmNormedList)]])
+}
+
 calcGroupRatios <- function(x,group,signal){
   #browser()
   medianInGroup <- median(x[[signal]][x[[group]]], na.rm=TRUE)
@@ -41,7 +83,7 @@ create8WellHistograms <- function(DT, pr, prDisplay, binwidth = diff(quantile(DT
 }
 
 ubHeatmap <- function(DT, title = NULL, cols = plateCol, activeThresh = .95) {
-  browser()
+  #browser()
   DT$Barcode <- as.factor(DT$Barcode)
   #Get medians of high serum numeric features
   fvDTHS <- DT[grepl("FBS", DT$MEP)]
@@ -78,4 +120,64 @@ ubHeatmap <- function(DT, title = NULL, cols = plateCol, activeThresh = .95) {
             lwid=c(1.5,0.2,2.5,2.5),mar=c(20,5), RowSideColors=cols[activeFV$Barcode], colRow = cols[activeFV$Barcode])
   
   return(activeFV)
+}
+
+heatmapNoBC <- function(DT, title = NULL, cols = plateCol, activeThresh = .95) {
+
+  activeFV <- DT
+  #Remove MEP and Barcode columns and convert to a matrix
+  activeFVM <- as.matrix(activeFV[,grep("MEP|Barcode",colnames(activeFV),invert=TRUE), with=FALSE])
+  
+  #This assignment of names retains the order after matrix coercion
+  rownames(activeFVM) <- names(DT$MEP)
+  
+  #Cluster the active MEPs, scaling the inputs
+  #plot.new()
+  heatmap.2(activeFVM,scale="column", col = bluered, trace = "none", cexRow=.5, cexCol=.9, key=FALSE, main = paste(title), lmat=rbind(c(5,0,4,0),c(3,1,2,0)), lhei=c(2.0,5.0),
+            lwid=c(1.5,0.2,2.5,2.5),mar=c(20,5), RowSideColors=cols[activeFV$Barcode], colRow = cols[activeFV$Barcode])
+  
+  return(activeFV)
+}
+
+findPerimeterCell <- function(x){
+  #browser()
+  if(!nrow(x)==0){
+    perimeterLogicals <- vector(length=nrow(x))
+    perimeterLogicals[which.max(x$Nuclei_PA_AreaShape_Center_R)] <- TRUE
+  }
+  return(perimeterLogicals)
+}
+
+labelOuterCells <- function(x, thresh=.75){
+  #browser()
+  outerLogicals <- NULL
+  if(!length(x)==0){
+    outerLogicals <- x>quantile(x,probs = thresh, na.rm=TRUE)
+  }
+  return(outerLogicals)
+}
+
+
+kmeansDNACluster <- function (x, centers = 2) 
+{
+  #browser()
+  x <- data.frame(x)
+  xkmeans <- kmeans(x, centers = centers)
+  if(centers==2){
+    if(xkmeans$centers[1] > xkmeans$centers[2]){
+      tmp <- xkmeans$cluster == 1
+      xkmeans$cluster[xkmeans$cluster == 2] <- 1L
+      xkmeans$cluster[tmp] <- 2L
+    }
+  }
+  return(xkmeans[["cluster"]])
+}
+
+lm_eqn <- function(df){
+  m <- lm(y ~ x, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
 }
