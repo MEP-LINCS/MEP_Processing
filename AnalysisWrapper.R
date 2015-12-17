31bed63611d121bc0c70b11ddb55b7f343f5cc33
Mark Dane <dane@ohsu.edu>
2015-12-16 17:28:01 -0800

    Set min and max on SCC heat maps

--- AnalysisWrapper.R
+++ AnalysisWrapper.R
@@ -3,6 +3,6 @@ library("rmarkdown")
 analysisVersion <- "v1"
 for(cellLine in c("MCF7","PC3","YAPC" )){
   for(ss in c("SS1","SS2","SS3")){
-    render("MEP-LINCS_Analysis.Rmd", output_file = paste0("MEP-LINCS_Analysis_",cellLine,"_",analysisVersion,"_",ss,".html"), output_format = "html_document") 
+    render("MEP-LINCS_Analysis.Rmd", output_file = paste0("./AnalysisReports/MEP-LINCS_Analysis_",cellLine,"_",analysisVersion,"_",ss,".html"), output_format = "html_document") 
   }
 }
