bdabcee69df140d1ef132b01a034f44901d52b0f
Mark Dane <dane@ohsu.edu>
2016-01-08 08:59:06 -0800

    Minor cleanup

--- AnalysisWrapper.R
+++ AnalysisWrapper.R
@@ -9,7 +9,7 @@ library("rmarkdown")
 
 analysisVersion <- "v1.31"
 for(cellLine in c("MCF7","PC3","YAPC")[2]){
-  for(ss in c("SS1","SS2","SS3")[3]){
+  for(ss in c("SS1","SS2","SS3")[2]){
     render("MEP-LINCS_Analysis.Rmd", output_file = paste0("./AnalysisReports/MEP-LINCS_Analysis_",cellLine,"_",analysisVersion,"_",ss,".html"), output_format = "html_document") 
   }
 }
