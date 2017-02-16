2492748f66d2834059a9eeef84d3c1436c67ab3a
markdane <dane@ohsu.edu>
2017-02-16 14:06:30 -0800

    minor cleanup on staining sets and file orgainzation

--- Release/MEP-LINCS_ReportsWrapper.R
+++ Release/MEP-LINCS_ReportsWrapper.R
@@ -2,7 +2,7 @@ library("rmarkdown")
 
 renderQACellReport <- function(path, datasetName){
   render("MEP_LINCS/Release/MEP-LINCS_QACellLevel.Rmd",
-         output_file = paste0(path,"/",datasetName,"/Reports/MEP-LINCS_QA_Cell_",datasetName,".html"),
+         output_file = paste0(path,"/study/",datasetName,"/Reports/MEP-LINCS_QA_Cell_",datasetName,".html"),
          output_format = "html_document")
 }
 
