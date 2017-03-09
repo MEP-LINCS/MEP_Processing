78d0d685a3319ede799df7ea17d305ef8970ec1f
markdane <dane@ohsu.edu>
2017-03-08 17:10:37 -0800

    Deleting Devel and Synpase folder files

--- Devel/MEP-LINCS_QA_CellCountWrapper.R
+++
@@ -1,9 +0,0 @@
-library("rmarkdown")
-
-analysisVersion <- "v1.3"
-for(cellLine in c("MCF7","PC3","YAPC")[2]){
-  for(ss in c("SS1","SS2noH3","SS2","SS3","SS0")[5]){
-    render("MEP-LINCS_QA_CellCount.Rmd", output_file = paste0("./QAReports/MEP-LINCS_QA_",cellLine,"_",analysisVersion,"_",ss,".html"), output_format = "html_document") 
-  }
-}
-
