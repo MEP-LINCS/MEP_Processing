a2a9571d4b9dfcf842c73385952fe88ed3bb05f5
markdane <dane@ohsu.edu>
2017-03-16 15:51:13 -0700

    Adding more deleted files

--- Release/Load.R
+++
@@ -1,6 +0,0 @@
-#Loader for shiny image QA app
-library(data.table)
-# dt <- fread("/graylab/share/dane/MEP-LINCS/MEP_LINCS/AnnotatedData/MCF10A_SS2_Level1.txt")
-# cDT <- dt[,.(Barcode,Well,Spot,PrintSpot,ArrayRow,ArrayColumn,ImageID,ECMp,Ligand,Nuclei_CP_AreaShape_Area,Nuclei_CP_AreaShape_Center_X,Nuclei_CP_AreaShape_Center_Y,Spot_PA_SpotCellCount,Nuclei_PA_Gated_EdUPositive,Nuclei_PA_Cycle_State,Nuclei_PA_AreaShape_Neighbors)]
-# save(cDT,file = "/graylab/share/dane/MEMAImageQA/MCF10A_SS2.RData")
-load(file = "/graylab/share/dane/MEMAImageQA/MCF10A_SS2.RData")
