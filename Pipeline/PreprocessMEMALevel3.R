0147f241db6d32a81664e93ab616762a3f6c5e70
markdane <dane@ohsu.edu>
2017-03-22 14:35:07 -0700

    Added optparse command line args and options

--- Pipeline/PreprocessMEMALevel3.R
+++ Pipeline/PreprocessMEMALevel3.R
@@ -1,4 +1,5 @@
-#title: "MEP-LINCs Preprocessing"
+#!/usr/bin/env Rscript
+
 #author: "Mark Dane"
 # 2/2017
 
@@ -10,33 +11,62 @@ library(ruv)
 library(stringr)
 library(tidyr)
 library(readr)
-library(dplyr)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(optparse))
 
-processLevel3CommandLine <- function(x, path, k=256, verbose="FALSE"){
-  if(length(x)<2) stop("There must be studyName and path arguments in the command line call.")
-  studyName <- x[1]
-  path <- x[2]
-  if((length(x)>2)) k <- x[3]
-  if((length(x)>3)) verbose <- x[4]
-  list(studyName, path, k, verbose)
+#' Get the command line arguments and options
+#' @return A list with options and args elements
+#'@export
+getL3CommandLineArgs <- function(){
+  option_list <- list(
+    make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+                help="Print extra output [default]"),
+    make_option(c("-q", "--quietly"), action="store_false",
+                dest="verbose", help="Print little output"),
+    make_option(c("-s", "--Synapse"), action="store_true", default=TRUE,
+                help="use Synpase for file accesses  [default]"),
+    make_option(c("-l", "--localServer"), action="store_false",
+                dest="Synapse", help="use a local server for file access"),
+    make_option(c("-w", "--writeFiles"), action="store_true", default=TRUE,
+                help="write output files to disk  [default]"),
+    make_option(c("-n", "--noWriteFiles"), action="store_false",
+                dest="writeFiles", help="do not write output to disk"),
+    make_option(c("-k", "--k"), type="integer", default=256,
+                help="Number of factors to use in RUV normalization [default %default]",
+                metavar="number")
+  )
+  parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
+  arguments <- parse_args(parser, positional_arguments = 2)
 }
 
-#callParams <- processLevel3CommandLine(c("MCF10A_DMSO_2", "/lincs/share/lincs_user",256,TRUE))
-#callParams <- processLevel3CommandLine(c("MCF10A_Neratinib_2", "/lincs/share/lincs_user",256,TRUE))
-#callParams <- processLevel3CommandLine(c("HMEC240L_SS4", "/lincs/share/lincs_user",256,TRUE))
-#callParams <- processLevel3CommandLine(c("HMEC122L_SS4", "/lincs/share/lincs_user",256,TRUE))
-#callParams <- processLevel3CommandLine(c("Neratinib_Dose_Response_Curve", "/lincs/share/lincs_user",256,TRUE))
-callParams <- processLevel3CommandLine(c("MCF10A_Cell_Titrate_8", "/lincs/share/lincs_user",0,TRUE))
+#Specify the command line options
+###Debug
+cl <-list(options=list(k=256,
+                       verbose=TRUE,
+                       Synapse=FALSE,
+                       writeFiles=FALSE),
+          args=c("HMEC122L_SS1", "/lincs/share/lincs_user"))
+####
 
-#callParams <- processLevel3CommandLine(commandArgs(trailingOnly = TRUE))
-studyName <-callParams[[1]]
-path <-callParams[[2]]
-k <- as.integer(callParams[[3]])
-verbose <- as.logical(callParams[[4]])
 startTime <- Sys.time()
+#Get and decode command line arguments and options
+cl <- getL3CommandLineArgs()
+studyName <- cl$args[1]
+path <- cl$args[2]
+
+opt <- cl$options
+k <- opt$k
+verbose <- opt$verbose
+useSynapse <- opt$Synapse
+writeFiles <- opt$writeFiles
+
 
 #Read the annotated data for all plates in the study
-slDT <- getSpotLevelData(studyName, path)
+if(useSynapse){
+  stop("Synapse not supported for level 3 data yet ")
+} else {
+  slDT <- getSpotLevelData(studyName, path)
+}
 
 signalsMinimalMetadata <- grep("_SE",grep("_CP_|_PA_|Barcode|^Well$|^Spot$|^PrintSpot$|^Ligand$|^ECMp$|^Drug$|^Drug1Conc$|^ArrayRow$|^ArrayColumn$|^CellLine$",colnames(slDT), value=TRUE), value=TRUE, invert=TRUE)
 
@@ -58,23 +88,15 @@ slDT <- QASpotLevelData(slDT, lowSpotCellCountThreshold=5,
                         lowRegionCellCountThreshold = 0.4,
                         lowWellQAThreshold = .7)
 
-if(verbose) message(paste("Writing level 3 file to disk\n"))
-fwrite(data.table(slDT), paste0(path, "/study/",studyName, "/Annotated/", studyName,"_Level3.tsv"), sep = "\t", quote=FALSE)
+if(writeFiles){
+  if(verbose) message(paste("Writing level 3 file to disk\n"))
+  if(useSynapse){
+    stop("Synapse not supported for level 3 data yet ")
+  } else {
+    fwrite(data.table(slDT), paste0(path, "/study/",studyName, "/Annotated/", studyName,"_Level3.tsv"), sep = "\t", quote=FALSE)
+  }
 
-# #Write the File Annotations for Synapse to tab-delimited file
-# annotations <- fread(paste0(path,"/",unique(slDT$Barcode)[1],"/Analysis/",unique(slDT$Barcode)[1],"_Level2Annotations.tsv"),header = FALSE)
-# 
-# write.table(c(
-#   CellLine = annotations$V2[annotations$V1=="CellLine"],
-#   Preprocess = annotations$V2[annotations$V1=="Preprocess"],
-#   DataType = annotations$V2[annotations$V1=="DataType"],
-#   Consortia = annotations$V2[annotations$V1=="Consortia"],
-#   Drug = annotations$V2[annotations$V1=="Drug"],
-#   Segmentation = annotations$V2[annotations$V1=="Segmentation"],
-#   StainingSet = annotations$V2[annotations$V1=="StainingSet"],
-#   Level = "3"),
-#   paste0(path,"/study/",studyName, "/Annotated/", studyName,"_","Level3Annotations.tsv"), sep = "\t",col.names = FALSE, quote=FALSE)
-
+}
 message(paste("Elapsed time to normalize ",studyName, Sys.time()-startTime, "\n"))
 
 
--- Pipeline/PreprocessMEMALevel4.R
+++ Pipeline/PreprocessMEMALevel4.R
@@ -1,47 +1,73 @@
-#title: "MEP-LINCs Preprocessing"
+#!/usr/bin/env Rscript
+
 #author: "Mark Dane"
 # 2/2017
 
-processLevel4CommandLine <- function(x, path, verbose="FALSE"){
-  if(length(x)<2) stop("There must be studyName and path arguments in the command line call.")
-  studyName <- x[1]
-  path <- x[2]
-  if((length(x)>2)) verbose <- x[3]
-  list(studyName, path, verbose)
+library(MEMA)
+suppressPackageStartupMessages(library(optparse))
+
+
+#' Get the command line arguments and options
+#' @return A list with options and args elements
+#'@export
+getL4CommandLineArgs <- function(){
+  option_list <- list(
+    make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+                help="Print extra output [default]"),
+    make_option(c("-q", "--quietly"), action="store_false",
+                dest="verbose", help="Print little output"),
+    make_option(c("-s", "--Synapse"), action="store_true", default=TRUE,
+                help="use Synpase for file accesses  [default]"),
+    make_option(c("-l", "--localServer"), action="store_false",
+                dest="Synapse", help="use a local server for file access"),
+    make_option(c("-w", "--writeFiles"), action="store_true", default=TRUE,
+                help="write output files to disk  [default]"),
+    make_option(c("-n", "--noWriteFiles"), action="store_false",
+                dest="writeFiles", help="do not write output to disk")
+  )
+  parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
+  arguments <- parse_args(parser, positional_arguments = 2)
 }
-library(MEMA) #merge, annotate and normalize functions
 
-#callParams <- processLevel4CommandLine(c("MCF10A_DMSO_2", "/lincs/share/lincs_user/study", TRUE))
-#callParams <- processLevel4CommandLine(c("HMEC240L_SS4", "/lincs/share/lincs_user/study",TRUE))
-#callParams <- processLevel4CommandLine(c("HMEC122L_SS4", "/lincs/share/lincs_user/study",TRUE))
-#callParams <- processLevel4CommandLine(c("MCF10A_Cell_Titrate_96", "/lincs/share/lincs_user/study",TRUE))
-callParams <- processLevel4CommandLine(c("MCF10A_Cell_Titrate_8", "/lincs/share/lincs_user/study",TRUE))
-#callParams <- processLevel4CommandLine(commandArgs(trailingOnly = TRUE))
-studyName <-callParams[[1]]
-path <-callParams[[2]]
-verbose <- callParams[[3]]
+#Specify the command line options
+###Debug
+cl <-list(options=list(verbose=TRUE,
+                       Synapse=FALSE,
+                       writeFiles=FALSE),
+          args=c("HMEC122L_SS1", "/lincs/share/lincs_user/study"))
+####
+
 startTime <- Sys.time()
+#Get and decode command line arguments and options
+cl <- getL4CommandLineArgs()
+studyName <- cl$args[1]
+path <- cl$args[2]
 
-l3DT <- fread(paste0(path,"/",studyName,"/Annotated/",studyName,"_Level3.tsv"))
+opt <- cl$options
+verbose <- opt$verbose
+useSynapse <- opt$Synapse
+writeFiles <- opt$writeFiles
+
+startTime <- Sys.time()
+if(useSynapse){
+  stop("Synapse not supported for level 4 data yet")
+} else {
+  l3DT <- fread(paste0(path,"/",studyName,"/Annotated/",studyName,"_Level3.tsv"))
+}
 #Summarize to the MEP_Drug level
 mepDT <- preprocessLevel4(l3DT,seNames=c("DNA2N","SpotCellCount","EdU","MitoTracker","KRT","Lineage","Fibrillarin"))
 #Add in the barcodes for each MEP_Drug
 mepDT <- addBarcodes(dt3 = l3DT, dt4 = mepDT)
 # Add a QA flag for spots with few replicates
 mepDT$QA_LowReplicateCount <- mepDT$Spot_PA_ReplicateCount < 3
-if(verbose) message("Writing level 4 file to disk\n")
-fwrite(mepDT, paste0(path, "/",studyName, "/Annotated/", studyName,"_Level4.tsv"), sep = "\t", quote=FALSE)
-# #Write the File Annotations for Synapse to tab-delimited file
-# annotations <- fread(paste0(path,"/",studyName,"/Annotated/",studyName,"_Level3Annotations.tsv"),header = FALSE)
-# write.table(c(
-#   CellLine = annotations$V2[annotations$V1=="CellLine"],
-#   Preprocess = annotations$V2[annotations$V1=="Preprocess"],
-#   DataType = annotations$V2[annotations$V1=="DataType"],
-#   Consortia = annotations$V2[annotations$V1=="Consortia"],
-#   Drug = annotations$V2[annotations$V1=="Drug"],
-#   Segmentation = annotations$V2[annotations$V1=="Segmentation"],
-#   StainingSet = annotations$V2[annotations$V1=="StainingSet"],
-#   Level = "4"),
-#   paste0(path,"/",studyName, "/Annotated/", studyName,"_","Level4Annotations.tsv"), sep = "\t",col.names = FALSE, quote=FALSE)
+
+if(writeFiles){
+  if(verbose) message("Writing level 4 file to disk\n")
+  if(useSynapse){
+    stop("Synpase not supported in level 4 data yet")
+  } else
+    fwrite(mepDT, paste0(path, "/",studyName, "/Annotated/", studyName,"_Level4.tsv"), sep = "\t", quote=FALSE)
+}
+
 if(verbose) message(paste("Elapsed time for ",studyName, "is", Sys.time()-startTime, "\n"))
 
