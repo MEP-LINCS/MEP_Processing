21f91c3f0d8226bbf41dfe3bd8a57874831287d0
markdane <dane@ohsu.edu>
2017-04-05 18:15:25 -0700

    including updates from Synapsifylevel3

--- Pipeline/PreprocessMEMALevel3.R
+++ Pipeline/PreprocessMEMALevel3.R
@@ -15,54 +15,75 @@ suppressPackageStartupMessages(library(optparse))
 
 # Get the command line arguments and options
 # returns a list with options and args elements
-getL3CommandLineArgs <- function(){
+getCommandLineArgs <- function(){
   option_list <- list(
     make_option(c("-v", "--verbose"), action="store_true", default=FALSE,
                 help="Print extra output"),
-    make_option(c("-l", "--local"), type="character", default=NULL,
-                help="Path to local input data directory if not using Synpase."),
     make_option(c("-k", "--k"), type="integer", default=256,
                 help="Number of factors to use in RUV normalization [default %default]",
-                metavar="number")
+                metavar="number"),
+    make_option(c("-i", "--inputPath"), type="character", default=NULL, metavar="PATH",
+                help="Path to local input data directory or Synapse ID for a File View."),
+    make_option(c("--synapseStore"), type="character", default=NULL, metavar="SYNAPSEID",
+                help="Store output file in Synapse directory (provide Synapse ID of Folder to store).")
   )
-  parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
+  parser <- OptionParser(usage = "%prog [options] STUDY OUTPUTFILE", option_list=option_list)
   arguments <- parse_args(parser, positional_arguments = 2)
 }
 
 #Specify the command line options
 ###Debug
 cl <-list(options=list(verbose=TRUE,
-                       local="/lincs/share/lincs_user",
-                       k=256),
+                       inputPath="syn7494072",
+                       k=256,
+                       synapseStore='syn8555683'),
           args=c("HMEC122L_SS1",
-                 "/lincs/share/lincs_user/study/HMEC122L_SS1/Annotated/HMEC122L_SS1_Level3.tsv")
-)
+                 "/tmp/HMEC122L_SS1_Level3.tsv"))
 ####
-cl <- getL3CommandLineArgs()
+cl <- getCommandLineArgs()
 
 studyName <- cl$args[1]
 ofname <- cl$args[2]
 
 opt <- cl$options
 verbose <- opt$verbose
-if(is.null(opt$local)){
-  useSynapse <- TRUE
-} else {
+
+if(file.exists(cl$options$inputPath)){
   useSynapse <- FALSE
-  path <- opt$local
+} else {
+  useSynapse <- TRUE
 }
+
 k <- opt$k
 
 startTime <- Sys.time()
 
 #Read the annotated data for all plates in the study
 if(useSynapse){
-  stop("Synapse not supported for level 3 data yet ")
+  suppressPackageStartupMessages(library(synapseClient))
+  suppressMessages(synapseLogin())
+  level <- "2"
+  levelQuery <- sprintf('SELECT id,Segmentation,Preprocess,Study,DataType,Consortia,StainingSet,CellLine,Drug from %s WHERE Study="%s" AND Level="%s"',
+                        cl$options$inputPath, studyName, level)
+  levelRes <- synTableQuery(levelQuery)
+  
+  dataPaths <- lapply(levelRes@values$id,
+                      function(x) getFileLocation(synGet(x)))
+  
 } else {
-  slDT <- getSpotLevelData(studyName, path)
+  barcodes <- getBarcodes(studyName)
+  dataPaths <- barcodes %>%
+    lapply(function(barcode, path){
+      paste0(path,"/",barcode,"/Analysis/",barcode,"_Level2.tsv")
+    }, path=opt$inputPath)
 }
 
-signalsMinimalMetadata <- grep("_SE",grep("_CP_|_PA_|Barcode|^Well$|^Spot$|^PrintSpot$|^Ligand$|^ECMp$|^Drug$|^Drug1Conc$|^ArrayRow$|^ArrayColumn$|^CellLine$",colnames(slDT), value=TRUE), value=TRUE, invert=TRUE)
+slDT <- getSpotLevelData(dataPaths)
+
+signalsMinimalMetadata <- grep("_SE",
+                               grep("_CP_|_PA_|Barcode|^Well$|^Spot$|^PrintSpot$|^Ligand$|^ECMp$|^Drug$|^Drug1Conc$|^ArrayRow$|^ArrayColumn$|^CellLine$",
+                                    colnames(slDT), value=TRUE), 
+                               value=TRUE, invert=TRUE)
 
 #RUVLoess normalize all signals
 if(!k==0){
@@ -85,8 +106,22 @@ slDT <- QASpotLevelData(slDT, lowSpotCellCountThreshold=5,
 if(verbose) message(paste("Writing level 3 file to disk\n"))
 fwrite(data.table(slDT), file=ofname, sep = "\t", quote=FALSE)
 
-if(useSynapse){
-  stop("Synapse not supported for level 3 data yet ")
+if(!is.null(cl$options$synapseStore)) {
+  if(verbose) message(sprintf("Writing to Synapse Folder %s", cl$options$synapseStore))
+  synFile <- File(ofname, parentId=opt$synapseStore)
+  synSetAnnotations(synFile) <- list(CellLine = unique(slDT$CellLine),
+                                     Study = unique(levelRes@values$Study),
+                                     Preprocess = unique(levelRes@values$Preprocess),
+                                     DataType = unique(levelRes@values$DataType),
+                                     Consortia = unique(levelRes@values$Consortia),
+                                     Drug = unique(levelRes@values$Drug),
+                                     Segmentation = unique(levelRes@values$Segmentation),
+                                     StainingSet = unique(levelRes@values$StainingSet),
+                                     Level = "3")
+  
+  synFile <- synStore(synFile,
+                      used=c(levelRes@values$id),
+                      forceVersion=FALSE)
 }
 
 message(paste("Elapsed time to normalize ",studyName, Sys.time()-startTime, "\n"))
