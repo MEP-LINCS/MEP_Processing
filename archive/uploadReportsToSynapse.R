fcc66557a7ce3d86396258ad992a5e0c8ed7539c
markdane <dane@ohsu.edu>
2017-04-06 17:55:00 -0700

    developing reports uploader

--- archive/uploadReportsToSynapse.R
+++ archive/uploadReportsToSynapse.R
@@ -12,6 +12,7 @@ synapseLogin()
 synapseAnnotatedDataDir <- "syn5713302"
 synapseReportDir <- "syn4939350"
 synapseMetadataDir <- "syn7213947"
+synapsePreReleaseView <- "syn7494072"
 
 #functions
 getFilePaths <- function(x){
@@ -31,8 +32,12 @@ studies <- data.frame(Study = rep(c("MCF10A_SS1",
                                         "HMEC240L_SS1",
                                         "HMEC240L_SS4"
 ),each=3),
-Report = c("Cell", "SpotMep", "Analysis"),
+Report = c("Cell", "SpotMEP", "Analysis"),
+Activity = c("QA Cell Level", "QA Spot and MEP Level", "Analyze Replicates"),
 stringsAsFactors=FALSE)
+studies <- rbind(studies, data.frame(Study=c("MCF10A_SSC","HMEC122L_SSC","HMEC240L_SSC"),
+                        Report="SSC",
+                        Activity="Analyze Cell Line"))
 
 #Get file paths from lincs server
 studies$FileName <- unlist(apply(studies, 1, getFilePaths))
@@ -40,36 +45,69 @@ studies$FileName <- unlist(apply(studies, 1, getFilePaths))
 #get permlink from GitHub
 repo <- getRepo("MEP-LINCS/MEP_LINCS", ref="branch", refName="develop")
 
+# Convert to a list to use as Synapse annotations
+toAnnotationList <- function(x) {
+  as.list(x %>% select(-c(filename, Activity)))
+}
+# Take row of data frame with filename and annots
+# Upload to Synapse and set annotations
+uploadToSynapse <- function(x, used, scriptLink, parentId) {
+  annots <- toAnnotationList(x)
+  obj <- File(x$filename, parentId=parentId)
+  synSetAnnotations(obj) <- annots
+  
+  obj <- synStore(obj, 
+                  activityName=x$Activity,
+                  used=used,
+                  forceVersion=FALSE,
+                  executed=scriptLink)
+  obj
+}
+
 uploadReports <- function(x){
-  path <- "/lincs/share/lincs_user/study"
   dataFile <- unique(data.frame(CellLine=x$CellLine,
                                 Drug=x$Drug,
                                 Preprocess=x$Preprocess,
                                 Segmentation=x$Segmentation,
-                                DataType=dataType,
-                                Consortia="MEP-LINCS",
+                                DataType=x$DataType,
+                                Consortia=x$Consortia,
                                 Level=x$Level,
-                                filename=x$FilePath,
+                                Activity=x$Activity,
+                                filename=x$FileName,
                                 stringsAsFactors = FALSE))
   #Separate used from annotations on the datafile
-  x <- filter(x,!grepl("SSC",StainingSet))
-  if(unique(x$Level=="QA_Cell")) used <- c(x[["1"]])
-  if(unique(x$Level=="QA_SpotMEP")) used <- c(x[["3"]],x[["4"]])
-  if(grepl("Analysis",unique(x$Level))) used <- c(x[["3"]],x[["4"]])
-  uploadToSynapseReports(dataFile, used=used, parentId=synapseReportDir)
+  if(unique(x$Level=="QA_Cell")){
+    used <- c(x[["1"]])
+    scriptLink <- getPermlink(repo, "Reports/MEP-LINCS_QACellLevel.Rmd")
+  } 
+  if(unique(x$Level=="QA_SpotMEP")){
+    used <- c(x[["3"]],x[["4"]])
+    scriptLink <- getPermlink(repo, "Reports/MEP-LINCS_QASpotMEPLevel.Rmd")
+  } 
+  if(grepl("Analysis",unique(x$Level))){
+    used <- c(x[["3"]],x[["4"]])
+    scriptLink <- getPermlink(repo, "Reports/MEP-LINCS_Analysis.Rmd")
+  }
+  if(grepl("SSC",unique(x$Level))){
+    used <- c(x[["3"]],x[["4"]])
+    scriptLink <- getPermlink(repo, "Reports/MEP-LINCS_AnalysisSB_SSC.Rmd")
+  }
+  uploadToSynapse(dataFile, used=used, scriptLink=scriptLink, parentId=synapseReportDir)
 }
 
-
-
 #Get files used from Synpase
-synIDs <- synQuery(paste("select id, name, Level, CellLine, StainingSet, Study from file where parentId=='",  synapseAnnotatedDataDir,"'"))
-colnames(synIDs)<-gsub("file.","",colnames(synIDs))
-synIDs <- filter(synIDs,!grepl("none|All",name))
-synIDs <- select(synIDs, -name)
-synIDs <- filter(synIDs, grepl("3|4|C",Level))
-synIDs <- dcast(synIDs,Study~Level,value.var="id")
-ssDatasets <- merge(synIDs,studies,by=c("Study"))
-###Debug need to have level 3 and 4 files before proceeding
-#Add annotations, used files and executed script link
+synIDs <- synTableQuery(sprintf('SELECT id,Level,StainingSet,Study,CellLine,Drug,Preprocess,Segmentation,DataType,Consortia from %s', synapsePreReleaseView))
+synIDs <- data.frame(synIDs@values)
+synIDs <- filter(synIDs, grepl("3|4",Level))
+used <- dcast(synIDs,Study~Level,value.var="id")
 
+#Add annotations, used files and executed script link
+studies$Study <- tolower(studies$Study)
+reports <- merge(used,studies,by=c("Study"))
+reports$Level <-reports$Report
+#Creat a level for each reports
+reports$Level[reports$Level %in% c("Cell","SpotMEP")] <- paste0("QA_",reports$Level[reports$Level %in% c("Cell","SpotMEP")])
+reportsAnnotated <- unique(merge(reports,select(synIDs,-c(id,Level)),by="Study"))
 #Upload file with annotations, used files and executed script
+res <- dlply(reportsAnnotated,"FileName", uploadReports)
+
