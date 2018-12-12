8c1d9e4aee267745ec16a355c9f06532d782cc53
markdane <dane@ohsu.edu>
2018-12-12 07:30:35 -0800

    error trapping for cell count file

--- Pipeline/Preprocess_MEMA_cell_counts.R
+++ Pipeline/Preprocess_MEMA_cell_counts.R
@@ -7,13 +7,17 @@ barcodes <- c("LI8X00907", "LI8V01150")
 addCellCounts <- function(barcode){
   data_dir <- "/lincs/share/lincs_user/"
   l2 <- read_tsv(paste0(data_dir, barcode, "/Analysis/",barcode,"_Level2.tsv"))
-  count_filename <- paste0(data_dir,barcode,"/Analysis/Count.txt")
-  if(!file.exists(count_filename)) stop(paste0(count_filename, " file not found"))
-  cell_counts <- read_delim(count_filename, "\"", escape_double = FALSE, trim_ws = TRUE) %>%
-    mutate(ImageID = Name,
-           Cell_count = X3) %>%
-    select(ImageID, Cell_count) %>%
+  
+  count_filenames <- dir(paste0(data_dir,barcode,"/Analysis/"), pattern="Count.*.txt", full.names = TRUE)
+  res <- lapply(count_filenames, function(fn){
+    cell_counts <- read_delim(fn, "\"", escape_double = FALSE, trim_ws = TRUE) %>%
+      mutate(ImageID = Name,
+             Cell_count = X3) %>%
+      select(ImageID, Cell_count)
+  }) %>%
+    bind_rows() %>%
     right_join(l2)
+
 }
 
 l2List <- lapply(barcodes, function(barcode){
