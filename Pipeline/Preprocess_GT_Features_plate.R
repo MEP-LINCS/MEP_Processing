d0111cac4ab839f4a95b3fc3671d80663c7288e8
markdane <dane@ohsu.edu>
2019-01-30 11:26:30 -0800

    Refactored for complete GT pipeline Added spatial sector+quadrant feature

--- Pipeline/Preprocess_GT_Features_plate.R
+++ Pipeline/Preprocess_GT_Features_plate.R
@@ -16,58 +16,45 @@ getCommandLineArgs <- function(){
   arguments <- parse_args(parser, positional_arguments = 1)
 }
 
+#Debug add signif reduction
+writeLevelData <- function(x, level){
+  write_csv(x, paste0(output_path,plateID,"/Analysis/GT1/",plateID,"_level",level,".csv"))
+}
+
 #Specify the command line options
-cl <- getCommandLineArgs()
-plateID <- cl$args[1]
-#plateID <- "LI8V01171"
-message("processing plate: ",plateID)
+if(!interactive()){
+  cl <- getCommandLineArgs()
+  input_path <- cl$args[1]
+  output_path <- cl$args[2]
+  plate_ID <- cl$args[3]
+} else {
+  input_path <- "/lincs/share/lincs_user/"
+  output_path <- "/lincs/share/lincs_user/"
+  plateID <- "LI8V01171"
+}
+
+message("processing GT1 data in plate: ",input_path,plateID)
 
 #get feature files
-files <- tibble(Full_filename = dir(plateID, pattern = ".txt",full.names = TRUE, recursive = TRUE)) %>%
+files <- tibble(Full_filename = dir(  input_path <- paste0(input_path,plateID,"/Analysis/GT1/"), pattern = ".csv",full.names = TRUE)) %>%
   mutate(Filename = str_remove(Full_filename, ".*/"),
          PlateID = str_remove(Filename, "_.*"),
-         Well = str_extract(Filename, "_[[:alpha:]][[:digit:]]+_"),
+         Well = str_extract(Filename, "_[[:digit:]]+_"),
          Well = str_remove_all(Well, "_"),
-         Spot = str_remove_all(Filename, ".*_|.txt"),
-         Spot = as.integer(Spot),
-         Well_Spot = paste0(Well,"_",Spot),
-         Compartment = str_extract(Filename,"Cells|Cytoplasm|Nuclei"),
-         Marker = str_remove_all(Filename,"^([^_]*_){3}|_.*"))
-
-well_spots <- unique(files$Well_Spot)
-addPrefix <-function(x, compartment, marker) paste0(compartment,"_GT_",marker,"_",x)
+         Well = as.integer(Well),
+         Well = wellAN(2,4)[Well])
 
 #Read in raw data with each row a cell
 #0 area cyto compartments result in NA values in Cyto features
-start_time <- Sys.time()
-raw_data <- lapply(well_spots, function(well_spot){
-  file_set <- files %>%
-    filter(Well_Spot == well_spot)
-  foo <- lapply(1:nrow(file_set), function(i){
-    rd <- suppressWarnings(suppressMessages(read_delim(file_set$Full_filename[i],delim = " ",col_names = TRUE))) %>%
-      select(-starts_with("X")) %>%
-      select(-matches("Haralick|_PS_|_LBP_|_Rank")) %>%
-      rename_all(.funs=addPrefix,compartment=file_set$Compartment[i],marker=file_set$Marker[i])
-    rd$ObjectNumber <- rd[[paste0(file_set$Compartment[i],"_GT_",marker=file_set$Marker[i],"_Label")]]
-    rd <- rd %>%
-      select(-matches(paste0(file_set$Compartment[i],"_GT_",marker=file_set$Marker[i],"_Label"))) 
-  }) %>% 
-    plyr::join_all(by="ObjectNumber") %>%
-    mutate(Well_Spot = well_spot,
-           Well = str_remove(Well_Spot,"_.*"),
-           Spot = str_remove(Well_Spot, ".*_"),
-           Spot = as.integer(Spot))
-  return(foo)
-}) %>%
-  bind_rows()
-Sys.time()-start_time
+raw_data <- map(files$Full_filename,read_csv) %>%
+  bind_rows
 
 #Read in the metadata
-  inputPath <- "/lincs/share/lincs_user"
-  metadataFiles <- list(annotMetadata=paste0(inputPath,"/",plateID,"/Analysis/",plateID,"_an2omero.csv"))
-  metadata <- MEMA::getMetadata(metadataFiles, TRUE)
+  metadataFiles <- list(annotMetadata=paste0(input_path, plateID, "/Analysis/", plateID, "_an2omero.csv"))
+  metadata <- MEMA::getMetadata(metadataFiles, TRUE) %>%
+    mutate_if(is.logical, as.character)
   
-  imageIDs <-  paste0(inputPath,"/",plateID,"/Analysis/",plateID,"_imageIDs.tsv") %>%
+  imageIDs <-  paste0(input_path, plateID,"/Analysis/", plateID,"_imageIDs.tsv") %>%
     read_tsv(col_types = cols(
       PlateID = col_character(),
       WellName = col_character(),
@@ -81,16 +68,17 @@ Sys.time()-start_time
            Well = c("A01","A02","A03","A04","B01","B02","B03","B04")[WellIndex],
            Spot = ArrayColumn+(ArrayRow-1)*20) %>%
     select(-WellName)
-  
+
   #merge the metadata with the ImageIDs
   full_metadata <- full_join(metadata,imageIDs, by = c("Well", "Spot", "WellIndex", "ArrayRow", "ArrayColumn"))
 
 level1_data <- raw_data %>%
-  inner_join(full_metadata, by=c("Well", "Spot")) %>%
-  mutate(Nuclei_CP_Intensity_MedianIntensity_Dapi = Nuclei_GT_Dapi_MeanIntensity,
+  inner_join(full_metadata, by=c("ImageID")) %>%
+  mutate(ObjectNumber = Label,
+         Nuclei_CP_Intensity_MedianIntensity_Dapi = Nuclei_GT_Dapi_MeanIntensity,
          Nuclei_CP_Intensity_IntegratedIntensity_Dapi = Nuclei_GT_Dapi_MeanIntensity*Nuclei_GT_Dapi_Surface,
-         Nuclei_CP_Intensity_MedianIntensity_EdU = Nuclei_GT_EdU_MeanIntensity,
-         Nuclei_CP_Intensity_IntegratedIntensity_EdU = Nuclei_GT_EdU_MeanIntensity*Nuclei_GT_EdU_Surface,
+         Nuclei_CP_Intensity_MedianIntensity_EdU = Cells_GT_EdU_MeanIntensity,
+         Nuclei_CP_Intensity_IntegratedIntensity_EdU = Cells_GT_EdU_MeanIntensity*Cells_GT_EdU_Surface,
          Cytoplasm_CP_Intensity_MedianIntensity_KRT14 = Cytoplasm_GT_KRT14_MeanIntensity,
          Cytoplasm_CP_Intensity_IntegratedIntensity_KRT14 = Cytoplasm_GT_KRT14_MeanIntensity*Cytoplasm_GT_KRT14_Surface,
          Cytoplasm_CP_Intensity_IntegratedIntensity_VIM = Cytoplasm_GT_VIM_MeanIntensity*Cytoplasm_GT_VIM_Surface,
@@ -99,13 +87,43 @@ level1_data <- raw_data %>%
   data.table::data.table() %>%
   gateCells()
 
-writeLevelData <- function(x, level){
-  #Make a v4 directory if needed
-  eppec_data_path <- getwd()
-  output_path <- paste0(eppec_data_path,"/",plateID,"/Analysis")
-  if(!dir.exists(output_path)) dir.create(output_path)
-  write_csv(x, paste0(output_path,"/",plateID,"_level",level,".csv"))
+#assign sectors
+assign_sectors <- function(df){
+  #assign a sector  based on concentric regions of equal area
+  #work in pixels where 1600 pixels = 500 um
+  spot_rad <- 200*(1600/500)
+  mid_rad <- sqrt(spot_rad^2/1.5)
+  ctr_rad <- sqrt(mid_rad^2/2)
+  plate_median_x <-  median(level1_data$Nuclei_GT_Dapi_CentroidX)
+  plate_median_y <-  median(level1_data$Nuclei_GT_Dapi_CentroidY)
+  spot_center_tol <- 50
+  
+  #convert to polar coordinates
+  #let the spot center wander +/-50 pixels about the typical center
+  #set sector logicals
+  df_with_sectors <- df %>%
+    group_by(Well, Spot) %>%
+    mutate(Spot_center_x = median(Nuclei_GT_Dapi_CentroidX),
+           Spot_center_x = scales::squish(Spot_center_x, c(plate_median_x-spot_center_tol, plate_median_x+spot_center_tol)),
+           Spot_center_y = median(Nuclei_GT_Dapi_CentroidY),
+           Spot_center_y = scales::squish(Spot_center_y, c(plate_median_y-spot_center_tol,plate_median_y+spot_center_tol)),
+           Nuclei_GT_Dapi_SpotCentroidX = Nuclei_GT_Dapi_CentroidX-Spot_center_x,
+           Nuclei_GT_Dapi_SpotCentroidY = Nuclei_GT_Dapi_CentroidY-Spot_center_y,
+           Nuclei_GT_Dapi_SpotCentroidR = sqrt(Nuclei_GT_Dapi_SpotCentroidX^2+Nuclei_GT_Dapi_SpotCentroidY^2),
+           Nuclei_GT_Dapi_SpotCentroidTheta = atan2(Nuclei_GT_Dapi_SpotCentroidY, Nuclei_GT_Dapi_SpotCentroidX)*180/pi,
+           Nuclei_GT_spatial_SectorCenter = Nuclei_GT_Dapi_SpotCentroidR <= ctr_rad,
+           Nuclei_GT_spatial_SectorMiddle = Nuclei_GT_Dapi_SpotCentroidR > ctr_rad&Nuclei_GT_Dapi_SpotCentroidR <= mid_rad,
+           Nuclei_GT_spatial_SectorOuter = Nuclei_GT_Dapi_SpotCentroidR > mid_rad & Nuclei_GT_Dapi_SpotCentroidR <= spot_rad,
+           Nuclei_GT_spatial_Quadrant1 = Nuclei_GT_Dapi_SpotCentroidTheta >= 0 & Nuclei_GT_Dapi_SpotCentroidTheta < 90,
+           Nuclei_GT_spatial_Quadrant2 = Nuclei_GT_Dapi_SpotCentroidTheta >= 90,
+           Nuclei_GT_spatial_Quadrant3 = Nuclei_GT_Dapi_SpotCentroidTheta <= -90,
+           Nuclei_GT_spatial_Quadrant4 = Nuclei_GT_Dapi_SpotCentroidTheta < 0 & Nuclei_GT_Dapi_SpotCentroidTheta > -90
+           ) %>%
+  ungroup() %>%
+    data.table::data.table() 
 }
+
+level1_data <- assign_sectors(level1_data)
 
 #Add proportions for signals with multivariate gating and non-conforming gate values
 addSpotProportions(level1_data)
@@ -121,24 +139,39 @@ if(length(gatedSignals)>0){
   level1_data <- left_join(level1_data, proportions, by = c("Barcode", "Well", "Spot"))
 }
 
-writeLevelData(level1_data,1)
+writeLevelData(level1_data, 1)
 
 level2_numeric_data <- level1_data %>%
-  group_by(Barcode,Well,Spot) %>%
+  group_by(Well,Spot) %>%
   mutate(Spot_PA_SpotCellCount=n()) %>%
-  summarise_if(is.numeric, median) 
-# %>%
+  summarise_if(is.numeric, median)  %>%
+  ungroup()
 #   mutate_if(is.numeric, signif, digits=4)
 
 level2_character_data <- level1_data %>%
-  group_by(Barcode,Well,Spot) %>%
-  summarise_if(is.character, unique)
+  group_by(Well,Spot) %>%
+  summarise_if(is.character, unique) %>%
+  ungroup()
 
-level2_logical_data <- level1_data %>%
-  group_by(Barcode,Well,Spot) %>%
-  summarise_if(is.logical, unique)
+# level2_logical_data <- level1_data %>%
+#   group_by(Well,Spot) %>%
+#   summarise_if(is.logical, unique) %>%
+#   ungroup()
 
-level2_data <- full_join(level2_character_data,level2_numeric_data, by = c("Barcode", "Well", "Spot")) %>%
-  right_join(level2_logical_data, by = c("Barcode", "Well", "Spot"))
+level2_spatial_data <- level1_data %>%
+    group_by(Well, Spot) %>%
+    summarise(Nuclei_GT_spatial_SectorCenterProportion = sum(Nuclei_GT_spatial_SectorCenter)/n(),
+              Nuclei_GT_spatial_SectorMiddleProportion = sum(Nuclei_GT_spatial_SectorMiddle)/n(),
+              Nuclei_GT_spatial_SectorOuterProportion = sum(Nuclei_GT_spatial_SectorOuter)/n(),
+              Nuclei_GT_spatial_Quadrant1Proportion = sum(Nuclei_GT_spatial_Quadrant1)/n(),
+              Nuclei_GT_spatial_Quadrant2Proportion = sum(Nuclei_GT_spatial_Quadrant2)/n(),
+              Nuclei_GT_spatial_Quadrant3Proportion = sum(Nuclei_GT_spatial_Quadrant3)/n(),
+              Nuclei_GT_spatial_Quadrant4Proportion = sum(Nuclei_GT_spatial_Quadrant4)/n()
+              ) %>%
+  ungroup()
 
+level2_data <- full_join(level2_character_data, level2_numeric_data, by = c("Well", "Spot")) %>%
+  #right_join(level2_logical_data, by = c("Well", "Spot")) %>%
+  right_join(level2_spatial_data, by = c("Well", "Spot"))
+  
 writeLevelData(level2_data,2)
